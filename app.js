68e86077d6dd622e4099c8ba25cc6d353c3f4880
devshivaji <141412513+devshivaji@users.noreply.github.com>
2023-08-25 16:27:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send({"msg":"hello"})
-  
 };
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
 
 
